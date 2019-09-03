5f4234b127854ab2428e999f2288e3e419cf7bb8
1458663235 <1458663235@qq.com>
2019-09-03 09:00:47 +0800

    add comment

--- src/main/java/com/trgroup/sort/SortRunner.java
+++ src/main/java/com/trgroup/sort/SortRunner.java
@@ -6,14 +6,15 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
+
 @Slf4j
 public class SortRunner {
     public static void main(String[] args) {
-
+        //生成随机数组
         int[] array = ThreadLocalRandom.current().ints(0, 100000).limit(100000).toArray();
         log.info(Arrays.stream(array).mapToObj(s -> String.valueOf(s)).collect(Collectors.joining(",", "[", "]")));
-        CglibProxy proxy=new CglibProxy();
-        BubbleSort bubbleSorter=(BubbleSort)proxy.getProxy(BubbleSort.class);
+        CglibProxy proxy = new CglibProxy();
+        BubbleSort bubbleSorter = (BubbleSort) proxy.getProxy(BubbleSort.class);
         int[] resultArray = bubbleSorter.bubbleSort(array);
         log.info(Arrays.stream(resultArray).mapToObj(s -> String.valueOf(s)).collect(Collectors.joining(",", "[", "]")));
     }
